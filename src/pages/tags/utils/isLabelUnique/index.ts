0eb4b6f58b11e95a52e1596fcc055b72d1fbea0f
Vlad Dimov <vlad.dimov@spruiktec.com>
2024-04-26 12:30:38 +0100

    Add option to create new tag from tags pages

--- src/pages/tags/utils/isLabelUnique/index.ts
+++ src/pages/tags/utils/isLabelUnique/index.ts
@@ -1,9 +1,8 @@
 import { TagData } from '../../../../models/tag'
 
 export const isLabelUnique = (label: string, tags: TagData[]) => {
-  let tagLabel = label.trim()
-  const existsInCurrentTags = tags.find((e) => e.label === tagLabel)
-  if (existsInCurrentTags) {
+  const exists = tags.find((e) => e.label === label)
+  if (exists) {
     return false
   }
   return true
