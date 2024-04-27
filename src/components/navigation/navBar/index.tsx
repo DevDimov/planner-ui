24f9e56195a2da80eae5f8b8a1ee661bbd3772d6
Vlad Dimov <vlad.dimov@spruiktec.com>
2024-04-27 19:12:30 +0100

    Make navbar responsive

--- src/components/navigation/navBar/index.tsx
+++ src/components/navigation/navBar/index.tsx
@@ -5,23 +5,27 @@ import { ReactComponent as Logo } from '../../../assets/Kalendara.svg'
 
 export function NavBar() {
   return (
-    <nav className="mb-12 flex items-center justify-center gap-1">
-      <div className="m-auto flex w-4 items-center">
+    <nav className="mb-12 mt-6 flex flex-col items-center justify-center gap-1 md:mt-0 md:flex-row">
+      <Logo className="w-36 fill-slate-800" />
+      {/* <div className="m-auto flex w-4 items-center">
         <div className="absolute left-5 flex w-36">
           <Logo className="fill-slate-800" />
         </div>
+      </div> */}
+      <div className="mb-3 flex justify-center gap-1 md:mb-0 md:flex-grow md:flex-row">
+        <NavLink to="/">
+          {({ isActive }) => <NavButton isActive={isActive} text="Home" />}
+        </NavLink>
+        <NavLink to="/tags">
+          {({ isActive }) => <NavButton isActive={isActive} text="Tags" />}
+        </NavLink>
       </div>
-      <NavLink to="/">
-        {({ isActive }) => <NavButton isActive={isActive} text="Home" />}
-      </NavLink>
-      <NavLink to="/tags">
-        {({ isActive }) => <NavButton isActive={isActive} text="Tags" />}
-      </NavLink>
-      <div className="m-auto flex w-4 items-center">
+      {/* <div className="m-auto flex w-4 items-center">
         <div className="absolute right-5">
           <LogoutButton />
         </div>
-      </div>
+      </div> */}
+      <LogoutButton />
     </nav>
   )
 }
